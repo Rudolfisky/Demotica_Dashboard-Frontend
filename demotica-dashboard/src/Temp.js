920a8d5d62c313072ff79e1773697cf4e1f22987
Rudolfisky <33927733+Rudolfisky@users.noreply.github.com>
2021-11-12 16:16:56 +0100

    added a automated fetch request

---
+++ demotica-dashboard/src/Temp.js
@@ -0,0 +1,61 @@
+import React from 'react';
+
+class Temp extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        error: null,
+        isLoaded: false,
+        result: null,
+        interval: 10000
+      };
+    }
+  
+    componentDidMount() {
+        console.log('Temp Component Mounted')
+        this.updateTimer = setInterval(() => this.getData(), 10000);
+        
+        this.getData();
+    }
+
+    getData(){
+        console.log('Temp Component getData()')
+        fetch("http://localhost:8087/temps/test/reandom")
+        .then(res => res.json())
+        .then(
+          (result) => {
+            this.setState({
+              isLoaded: true,
+              result: result
+            });
+          },
+          // Note: it's important to handle errors here
+          // instead of a catch() block so that we don't swallow
+          // exceptions from actual bugs in components.
+          (error) => {
+            this.setState({
+              isLoaded: true,
+              error
+            });
+          }
+        )
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.interval);
+      }
+  
+    render() {
+      const { error, isLoaded, result } = this.state;
+      if (error) {
+        return <div>Error: {error.message}</div>;
+      } else if (!isLoaded) {
+        return <div>Loading...</div>;
+      } else {
+        return (
+            result
+        );
+      }
+    }
+  }
+  export default Temp
--- demotica-dashboard/src/index.js
+++ demotica-dashboard/src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Timer from './Timer';
+import Temp from './Temp';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Timer />
+    <Temp />
   </React.StrictMode>,
   document.getElementById('root')
 );
