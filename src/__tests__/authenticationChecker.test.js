316d76aacfe157b4039eeafd120059358fcc0b07
Rudolfisky <33927733+Rudolfisky@users.noreply.github.com>
2022-01-07 16:36:05 +0100

    uncommented tests in 2 test files

--- src/__tests__/authenticationChecker.test.js
+++ src/__tests__/authenticationChecker.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import AuthenticationChecker from '../Components/authenticationChecker';
 
-// test('renders login in button from authentication checker', () => {
-//   render(<AuthenticationChecker />);
-//   const linkElement = screen.getByTestId(/loginBTN/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
+test('renders login in button from authentication checker', () => {
+  render(<AuthenticationChecker />);
+  const linkElement = screen.getByTestId(/loginBTN/i);
+  expect(linkElement).toBeInTheDocument();
+});
