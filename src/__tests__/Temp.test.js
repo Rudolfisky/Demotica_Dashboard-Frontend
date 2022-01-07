4d8357892ef95388979c9c6581fd4adac6742b13
Rudolfisky <33927733+Rudolfisky@users.noreply.github.com>
2022-01-07 16:34:05 +0100

    returned 2 tests

---
+++ src/__tests__/Temp.test.js
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react';
+import Temp from '../Components/Temp';
+
+// test('renders temp', () => {
+//   render(<Temp />);
+//   const linkElement = screen.getByTestId(/temp/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
---
+++ src/__tests__/authenticationChecker.test.js
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react';
+import AuthenticationChecker from '../Components/authenticationChecker';
+
+// test('renders login in button from authentication checker', () => {
+//   render(<AuthenticationChecker />);
+//   const linkElement = screen.getByTestId(/loginBTN/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
